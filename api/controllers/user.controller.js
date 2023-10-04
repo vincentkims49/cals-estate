698f608f417336d8a174c4f5c1e19b654e8e7c75
vincentkims49 <vincentkims49@gmail.com>
2023-10-04 10:55:32 +0300

    add update user api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,7 +1,39 @@
-export const test= (re, res) =>{
-    res.json(
+import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+
+export const test = (re, res) => {
+  res.json({
+    message: "Api route is working!",
+  });
+};
+
+export const updateUser = async (req, res, next) => {
+    if (req.user.id !== req.params.id)
+      return next(errorHandler(401, 'You can only update your own account!'));
+    try {
+      if (req.body.password) {
+        req.body.password = bcryptjs.hashSync(req.body.password, 10);
+      }
+  
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
         {
-            message: "Api route is working!",
-        }
-    )
-}
+          $set: {
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+            avatar: req.body.avatar,
+          },
+        },
+        { new: true }
+      );
+  
+      const { password, ...rest } = updatedUser._doc;
+  
+      res.status(200).json(rest);
+    } catch (error) {
+      next(error);
+    }
+  };
+  
