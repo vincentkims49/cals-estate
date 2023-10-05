776ae064d133fa0547763688463e2ffaba592006
vincentkims49 <vincentkims49@gmail.com>
2023-10-05 11:16:07 +0300

    create user listings and minor changes

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -20,6 +20,8 @@ import {
 } from "../redux/user/userSlice";
 import { signOutUserStart } from "../redux/user/userSlice";
 
+import { Link } from "react-router-dom";
+
 export default function Profile() {
   const fileRef = useRef(null);
   const { currentUser, loading, error } = useSelector((state) => state.user);
@@ -178,6 +180,7 @@ export default function Profile() {
         >
           {loading ? "Loading..." : "Update"}
         </button>
+        <Link className="bg-green-700 rounded-lg text-white p-3 text-center uppercase hover: opacity-95" to={"/create-listing"}>Create Listing</Link>
       </form>
       <div className="flex justify-between mt-5">
         <span
