29fd1a0ad1d3bf5f19304534356bba4dae8ad499
vincentkims49 <vincentkims49@gmail.com>
2023-10-06 12:08:53 +0300

    add listing api

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -4,6 +4,7 @@ import {
   deleteListing,
   updateListing,
   getListing,
+  getListings,
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -13,6 +14,6 @@ router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/update/:id", verifyToken, updateListing);
 router.get('/get/:id', getListing);
-// router.get('/get', getListings);
+router.get('/get', getListings);
 
 export default router;
