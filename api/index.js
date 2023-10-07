742c47c72f7b0d3a3c7beb222e8e2783bb43107a
vincentkims49 <vincentkims49@gmail.com>
2023-10-07 17:10:23 +0300

    add pre installation

--- api/index.js
+++ api/index.js
@@ -9,6 +9,8 @@ import cookieParser from "cookie-parser";
 
 import listingRouter from "./routes/listing.route.js";
 
+import path from "path";
+
 dotenv.config();
 
 mongoose
@@ -20,6 +22,7 @@ mongoose
     console.log(err);
   });
 
+const __dirname = path.resolve();
 const app = express();
 
 app.use(express.json());
@@ -34,6 +37,10 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/dist/index.html"));
+},);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
